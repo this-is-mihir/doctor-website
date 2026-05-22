d85cf4c51ebf2a86509f473e189c5edd00fbaff9
Mihir Patel <patelmihir0367@gmail.com>
2026-05-22 16:01:01 +0530

    Change WhatsApp number in site.config.js
    Updated WhatsApp contact number in site configuration.

--- src/config/site.config.js
+++ src/config/site.config.js
@@ -14,7 +14,7 @@ export const SITE = {
   phoneAlt: "+91 97XXXXXXXX",
   email: "info@healthcareclinic.com",
   emailAppointment: "appointments@healthcareclinic.com",
-  whatsapp: "916354689922",
+  whatsapp: "916666558899",
   mapEmbedUrl: "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.0!2d72.5!3d23.0!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2sClinic!5e0!3m2!1sen!2sin!4v1234567890",
   social: {
     facebook: "#", instagram: "#", twitter: "#", youtube: "#", linkedin: "#",
